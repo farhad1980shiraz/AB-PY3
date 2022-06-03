4406ef3ee60cad3ac9efae3f641918acfdd5cb7e
farhad1980shiraz <106616090+farhad1980shiraz@users.noreply.github.com>
2022-06-03 19:32:24 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://www.google.com")
